c8b035b894af7205a71646242077a4d938698b1e
sajjad <sajjadsajjad098765@gmail.com>
2024-07-12 20:31:55 +0600

    first commit

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar/Navbar";
+
+const Root = () => {
+  return (
+    <div className="container mx-auto">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Root;
